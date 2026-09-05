da9fae1145f9e4963ddcc4565d809463c9420bfe
Smart-menu-solutions <smartmenusolutions@outlook.com>
2026-09-05 08:23:47 +0300

    Add console log for script loading

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("SCRIPT GELADEN");
 document.addEventListener('DOMContentLoaded', function () {
   var toggle = document.querySelector('.nav-toggle');
   var nav = document.querySelector('.main-nav');
